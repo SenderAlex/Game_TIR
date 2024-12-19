ae3a5dba7cfd0694524ab6a30e71d1ac055dd7d9
Alexander Sender <141014024+SenderAlex@users.noreply.github.com>
2024-12-19 23:13:41 +0300

    game graphics

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import pygame
-
+import random
 
 pygame.init()
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Игра ТИР')
+
+target_image = pygame.image.load("images/target.jpeg")
+TARGET_WIDTH = 80
+TARGET_HEIGHT = 60
+target_x = random.randint(0, SCREEN_WIDTH - TARGET_WIDTH)
+target_y = random.randint(0, SCREEN_HEIGHT - TARGET_HEIGHT)
 
 running = True
 while running:
-    pass
+    screen.fill(color)
 
 
 
